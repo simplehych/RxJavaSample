0b8810e2f8747dff57b63a836d94997eb4c93c91
hych <simplehych@gmail.com>
2019-01-21 18:42:39 +0800

    rxjava 上一级 下一级

--- app/src/main/java/com/simple/rxjavasample/Test.java
+++ app/src/main/java/com/simple/rxjavasample/Test.java
@@ -1,6 +1,5 @@
 package com.simple.rxjavasample;
 
-import android.text.format.DateFormat;
 import android.util.Log;
 import io.reactivex.*;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -28,53 +27,56 @@ public class Test {
     private String TAG = "Test";
 
     public void testCreate() {
-        Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                Log.i(TAG, "ObservableOnSubscribe emit 1");
-                emitter.onNext(1);
-                Log.i(TAG, "ObservableOnSubscribe emit 2");
-                emitter.onNext(2);
-                Log.i(TAG, "ObservableOnSubscribe emit 3");
-                emitter.onNext(3);
-                Log.i(TAG, "ObservableOnSubscribe emit 4");
-                emitter.onNext(4);
-                Log.i(TAG, "ObservableOnSubscribe onComplete");
-                emitter.onComplete();
-                Log.i(TAG, "ObservableOnSubscribe emit 5");
-                emitter.onNext(5);
-                Log.i(TAG, "ObservableOnSubscribe emit -");
-                Log.i(TAG, "ObservableOnSubscribe emit --");
+        Observable
+                .create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                        Log.i(TAG, "ObservableOnSubscribe emit 1");
+                        emitter.onNext(1);
+                        Log.i(TAG, "ObservableOnSubscribe emit 2");
+                        emitter.onNext(2);
+                        Log.i(TAG, "ObservableOnSubscribe emit 3");
+                        emitter.onNext(3);
+                        Log.i(TAG, "ObservableOnSubscribe emit 4");
+                        emitter.onNext(4);
+                        Log.i(TAG, "ObservableOnSubscribe onComplete");
+                        emitter.onComplete();
+                        Log.i(TAG, "ObservableOnSubscribe emit 5");
+                        emitter.onNext(5);
+                        Log.i(TAG, "ObservableOnSubscribe emit -");
+                        Log.i(TAG, "ObservableOnSubscribe emit --");
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
 
-            }
-        }).subscribe(new Observer<Integer>() {
+                    private Disposable mDisposable;
 
-            private Disposable mDisposable;
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.i(TAG, "Observer onSubscribe " + d.isDisposed());
+                        mDisposable = d;
+                    }
 
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.i(TAG, "Observer onSubscribe " + d.isDisposed());
-                mDisposable = d;
-            }
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "Observer onNext " + integer);
+                        if (integer == 2) {
+                            mDisposable.dispose();
+                        }
+                    }
 
-            @Override
-            public void onNext(Integer integer) {
-                Log.i(TAG, "Observer onNext " + integer);
-                if (integer == 2) {
-                    mDisposable.dispose();
-                }
-            }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "Observer onError " + e.getMessage());
+                    }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "Observer onError " + e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i(TAG, "Observer onComplete ");
-            }
-        });
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "Observer onComplete ");
+                    }
+                });
     }
 
     public void testMap() {
