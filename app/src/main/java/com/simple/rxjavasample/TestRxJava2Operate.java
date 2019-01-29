209d0c12a69481675368d9b6f5e9a8fc1c73bf11
hych <simplehych@gmail.com>
2019-01-29 23:32:53 +0800

    测试 RxJava2 操作符

--- app/src/main/java/com/simple/rxjavasample/TestRxJava2Operate.java
+++ app/src/main/java/com/simple/rxjavasample/TestRxJava2Operate.java
@@ -3,11 +3,15 @@ package com.simple.rxjavasample;
 import android.util.Log;
 import io.reactivex.*;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.observables.GroupedObservable;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
@@ -190,4 +194,163 @@ public class TestRxJava2Operate {
             }
         }).startWith("1");
     }
+
+    public void testTimeout() {
+        Observable
+                .create(new ObservableOnSubscribe<Long>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Long> emitter) throws Exception {
+                        emitter.onNext(1L);
+                        emitter.onNext(2L);
+                        Thread.sleep(600);
+                        emitter.onNext(3L);
+                        emitter.onNext(4L);
+                    }
+                })
+                .timeout(500, TimeUnit.MILLISECONDS, Observable.just(999L))
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Log.i(TAG, "testTimeout: " + aLong);
+                    }
+                });
+    }
+
+    public void testCatch() {
+
+        Observable
+                .create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                        emitter.onNext(1);
+                        emitter.onNext(2);
+                        emitter.onError(new Exception());
+                        emitter.onNext(3);
+                    }
+                })
+//                .onErrorReturnItem(666)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "testCatch: " + integer);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.i(TAG, "testCatch: " + throwable);
+                    }
+                }, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Log.i(TAG, "testCatch Action ");
+                    }
+                });
+    }
+
+    public void testRetry() {
+        Observable
+                .create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                        emitter.onNext(1);
+                        emitter.onNext(2);
+                        emitter.onError(new Throwable("Error1"));
+                        //                        emitter.onError(new Throwable("Error2"));
+                        emitter.onNext(3);
+                    }
+                })
+                .retry(2, new Predicate<Throwable>() {
+                    @Override
+                    public boolean test(Throwable throwable) throws Exception {
+                        return true;
+                    }
+                })
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.i(TAG, "testRetry onSubscribe " + d.isDisposed());
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "testRetry onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "testRetry onError: " + e);
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "testRetry onComplete");
+                    }
+                });
+    }
+
+    public void testAll() {
+        Observable.range(5, 5)
+                .all(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer > 5;
+                    }
+                })
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        Log.i(TAG, "testAll " + aBoolean);
+                    }
+                });
+    }
+
+    public void testIsEmpty() {
+        Observable
+                .range(5, 0)
+                .isEmpty()
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        Log.i(TAG, "testIsEmpty " + aBoolean);
+                    }
+                });
+    }
+
+    public void testSequenceEqual() {
+        Observable
+                .sequenceEqual(Observable.range(1, 5),
+                        Observable.range(1, 5));
+    }
+
+    public void testAmb() {
+        Observable
+                .amb(Arrays.asList(Observable.range(1, 5),
+                        Observable.range(6, 5)))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "testAmb " + integer);
+                    }
+                });
+    }
+
+    public void testToList() {
+        Observable
+                .range(1, 5)
+                .toSortedList(new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        Log.i(TAG, "testToList  o1: " + o1 + " o2: " + o2);
+                        return -o1;
+                    }
+                })
+                .subscribe(new Consumer<List<Integer>>() {
+                    @Override
+                    public void accept(List<Integer> integers) throws Exception {
+                        Log.i(TAG, "testToList " + integers);
+                    }
+                });
+    }
+
 }
